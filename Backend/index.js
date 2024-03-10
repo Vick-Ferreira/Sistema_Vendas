cf0baea6ee7bb525a64418066c260a99ec323d7b
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2024-03-10 15:51:22 -0300

    deploy

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,14 @@
 //config inicial chamando os pacotes instalador
 const express = require('express') //importando arquivos do node_modules
 const mongoose = require('mongoose')
-const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;//Heroku
 
+// Configuração do middleware express.static para servir arquivos estáticos
+const frontendPath = path.join(__dirname, '../Frontend');
+app.use(express.static(frontendPath));
 
-const app = express() //inicializando,executa o express como uma função 
-app.use(cors());
 
 //forma de ler JSON  - middlewares = recursos executados entre as requisições respostas
 //agora ele consegue ler as respostar em json
@@ -18,9 +21,6 @@ app.use(
 )
 
 app.use(express.json())
-
-
-
 
 //ROTAS
 const produtoRouter = require('./routes/produtoRoutes');
@@ -39,4 +39,25 @@ app.get('/', (req, res) => {
     //requisição
     res.json({message: 'Oi express'})//a resposta para a minha  rota / vai ser json e vou enviar algo, json = objeto javascript , paramentro message
 })//em react é necessario eu tratar  as respostas, verificar se veio parametro message e exibir a mensagem na tela  = CONECTAR AMBOS
+
+// Iniciando o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
+
+
+//conexão com o mongo
+//mongodb+srv://vitoriaferreirap06:123456@cluster0.6puyj2y.mongodb.net/bancodaapi?retryWrites=true&w=majority
+//ENTREGA UMA PORTA
+//não quero iniciar a aplicação antes de se conectar com o banco de dados - conectando ao banco
+const db_user = 'vitoria';
+const db_password = encodeURIComponent('Romeu20');
+
+mongoose
+  .connect(`mongodb+srv://${db_user}:${db_password}@api.fajqg6b.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log("Conectou ao MongoDB");
+
+  })
+  .catch((err) => console.error(err));
 
