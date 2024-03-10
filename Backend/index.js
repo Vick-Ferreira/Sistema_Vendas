e95a6a4d4f8431872242c037d83d37a97f6cb779
Vitoria Ferreira <vitoriaferreirap06@gmail.com>
2024-03-10 15:34:43 -0300

    sem mongoose

--- Backend/index.js
+++ Backend/index.js
@@ -40,20 +40,3 @@ app.get('/', (req, res) => {
     res.json({message: 'Oi express'})//a resposta para a minha  rota / vai ser json e vou enviar algo, json = objeto javascript , paramentro message
 })//em react é necessario eu tratar  as respostas, verificar se veio parametro message e exibir a mensagem na tela  = CONECTAR AMBOS
 
-//conexão com o mongo
-
-//mongodb+srv://vitoriaferreirap06:123456@cluster0.6puyj2y.mongodb.net/bancodaapi?retryWrites=true&w=majority
-
-//ENTREGA UMA PORTA
-//não quero iniciar a aplicação antes de se conectar com o banco de dados - conectando ao banco
-const db_user = 'vitoria';
-const db_password = encodeURIComponent('Romeu20');
-
-mongoose
-  .connect(`mongodb+srv://${db_user}:${db_password}@api.fajqg6b.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
-  .then(() => {
-    console.log("Conectou ao MongoDB");
-    app.listen(3000);
-  })
-  .catch((err) => console.error(err));
-
